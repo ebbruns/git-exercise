b082f10295bb656d522080eae4be95f7b1db3ed0
ebbruns <evan.bruns@gmail.com>
2014-01-21 10:59:29 -0600

    Update HelloWorld.java

--- src/edu/grinnell/csc207/brunseva/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/brunseva/hello/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld
   public static void 
   main (String[] args) 
 {
-  System.out.println ("Welcome to butts.net");
+  System.out.println ("Welcome to mudkips.net");
   System.out.println ("Hello there, friendo");
 } // main(String[])
 }
